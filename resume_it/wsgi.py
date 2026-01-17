17128b910f0d4c1527caef952d07b2d11c486d0c
Evgeny Pikhtulov <pihtulovevgeny@gmail.com>
2026-01-17 22:38:47 +0400

    push 3

--- resume_it/wsgi.py
+++ resume_it/wsgi.py
@@ -1,9 +1,7 @@
 """
 WSGI config for resume_it project.
 
-Production configuration with WhiteNoise for static files.
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
+Production configuration with WhiteNoise for static and media files.
 """
 
 import os
@@ -13,14 +11,11 @@ from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'resume_it.settings')
 
-# Импортируем settings после установки переменной окружения
-from django.conf import settings
-
 # Получаем стандартное WSGI приложение Django
 django_app = get_wsgi_application()
 
 # Оборачиваем в WhiteNoise для обслуживания статических файлов
-application = WhiteNoise(django_app, root=settings.STATIC_ROOT)
+application = WhiteNoise(django_app, root='/app/staticfiles')
 
-# Для медиа файлов (опционально, если веб-сервер не настроен)
-application.add_files(settings.MEDIA_ROOT, prefix=settings.MEDIA_URL)
+# Добавляем медиа файлы из /data/media
+application.add_files('/data/media', prefix='media')
