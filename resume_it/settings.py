04c32a950a18f86ffcc614bee3b8def194dd6f6e
Evgeny Pikhtulov <pihtulovevgeny@gmail.com>
2026-02-01 13:40:27 +0400

    push - изменение в настройках settings.py

--- resume_it/settings.py
+++ resume_it/settings.py
@@ -108,7 +108,8 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'  # Для collectstatic
 
 # Для WhiteNoise - сжимает статические файлы
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.WhiteNoiseStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = Path('/data/media')  # Для загружаемых файлов (photos, certificates)
